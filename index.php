2b44338a2cad35401b20c5f177758d723408b7b6
wohlig <chintan@wohlig.com>
2014-10-13 00:48:46 +0530

    Changes for the website

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" ng-app="firstapp">
 
 <head ng-controller="headerctrl">
+    <!-- start Mixpanel --><script type="text/javascript">(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
+                                                                                                                                                  for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2.2.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
+    mixpanel.init("adb4a5f089deeab61c0fecdfc4853d78");</script><!-- end Mixpanel -->
     <title ng-bind="'Lyla Loves - '+template.title">Lyla Loves</title>
     <script>
         var hostname=window.location.hostname;
@@ -157,6 +160,10 @@
     var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
     g.src = "https://js.gleam.io/aaA2s/ol.js"; s.parentNode.insertBefore(g, s);
 }(document, "script"));</script>
+    
+    <script src="//static.getclicky.com/js" type="text/javascript"></script>
+    <script type="text/javascript">try{ clicky.init(100777994); }catch(e){}</script>
+    <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100777994ns.gif" /></p></noscript>
     
 </body>
 
