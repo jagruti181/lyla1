d5d1ac354064740d0b7facb455aaa829a563dafe
Chintan Shah <chintan@wohlig.com>
2015-03-28 12:32:43 +0530

    git

--- admin/application/config/autoload.php
+++ admin/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','session','form_validation','cart','csvreader','MY_Session','hybridauthlib');
+$autoload['libraries'] = array('database','session','form_validation','cart','csvreader','MY_Session');
 
 
 /*
